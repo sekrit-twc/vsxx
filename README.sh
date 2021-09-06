9cfe9ca8ed97f770fe262d7b9f7c03d4941ba00b
sekrit-twc <noreply@example.com>
2021-09-05 20:39:17 -0700

    Move VS headers into subdirectory

--- README.sh
+++ README.sh
@@ -1,3 +1,3 @@
 # To compile example.
-c++ -std=c++11 -shared -I. -fPIC -fvisibility=hidden example/invert_example.cpp vsxx_pluginmain.cpp -o invert_example.so
-c++ -std=c++11 -I. -D__STDC_FORMAT_MACROS example/pipe_example.cpp -lvapoursynth-script -o pipe_example
+c++ -std=c++11 -shared -I. -IVapourSynth -fPIC -fvisibility=hidden example/invert_example.cpp vsxx_pluginmain.cpp -o invert_example.so
+c++ -std=c++11 -I. -IVapourSynth -D__STDC_FORMAT_MACROS example/pipe_example.cpp -lvapoursynth-script -o pipe_example
