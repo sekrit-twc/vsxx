e6c1b454050f104f8c613c29921ea20ffe15b713
sekrit-twc <noreply@example.com>
2021-09-05 20:52:36 -0700

    Explicitly request final APIv3 version

--- vsxx_pluginmain.cpp
+++ vsxx_pluginmain.cpp
@@ -2,9 +2,11 @@
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin config_func, VSRegisterFunction register_func, VSPlugin *plugin)
 {
+	constexpr int VS_API_3_6 = (3 << 16) | 6;
+
 	const PluginInfo &info = g_plugin_info;
 
-	config_func(info.identifier, info.plugin_namespace, info.plugin_name, VAPOURSYNTH_API_VERSION, info.read_only, plugin);
+	config_func(info.identifier, info.plugin_namespace, info.plugin_name, VS_API_3_6, info.read_only, plugin);
 	for (const auto &f : info.filters) {
 		register_func(f.name, f.arg_str, f.factory, f.construct_arg, plugin);
 	}
